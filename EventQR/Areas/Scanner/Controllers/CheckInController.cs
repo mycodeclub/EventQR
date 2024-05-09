658cf567163b442a7328823bf2019c2863cbb669
Piyush Srivastava <piyushsrivastava1999brh@gmail.com>
2024-05-09 15:47:54 +0530

    Added URL Encrypting and Decrypting for CheckInController

--- EventQR/Areas/Scanner/Controllers/CheckInController.cs
+++ EventQR/Areas/Scanner/Controllers/CheckInController.cs
@@ -6,11 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace EventQR.Areas.Scanner.Controllers
 {
     [Area("Scanner")]
-    [Authorize(Roles = "Scanner,EventOrganizer")]
+ //   [Authorize(Roles = "Scanner")]
     public class CheckInController : Controller
     {
         private readonly AppDbContext _context;
@@ -19,8 +20,37 @@ namespace EventQR.Areas.Scanner.Controllers
             _context = context;
         }
 
-
-        public async Task<IActionResult> AllowGuest(Guid guestId, Guid eventId)
+        public async Task<IActionResult> AllowGuestNew(string id)
+        {
+            var DecryptString = EventQR.Common.Static.Variables.Decrypt(id);
+            Guid guestId = Guid.Empty;
+            Guid eventId = Guid.Empty;
+            if (!string.IsNullOrWhiteSpace(DecryptString))
+            {
+                var ids = DecryptString.Split(",");
+                Guid.TryParse(ids[0], out guestId);
+                Guid.TryParse(ids[1], out eventId);
+            }
+            var guest = await _context.Guests.Include(g => g.MyEvent).Where(g => g.UniqueId == guestId && g.EventId == eventId).FirstOrDefaultAsync();
+            if (string.IsNullOrWhiteSpace(guest.AllowedSubEventsIdsCommaList))
+            {
+                var allowedSubEvents = guest.AllowedSubEventsIdsCommaList.Split(',').Select(Guid.Parse);
+                guest.SubEvents = _context.SubEvents.Where(e => allowedSubEvents.Contains(e.UniqueId)).ToList();
+            }
+            GuestCheckIn _checkin = new GuestCheckIn()
+            {
+                UserLoginId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier),
+                GuestId = guestId,
+                Guest = guest,
+                EventId = eventId,
+                Event = guest.MyEvent,
+                CheckIn = DateTime.Now
+            };
+            await _context.CheckIns.AddAsync(_checkin);
+            await _context.SaveChangesAsync();
+            return View(_checkin);
+        }
+        public async Task<IActionResult> AllowGuestOld(Guid guestId, Guid eventId)
         {
             var guest = await _context.Guests.Include(g => g.MyEvent).Where(g => g.UniqueId == guestId && g.EventId == eventId).FirstOrDefaultAsync();
             if (string.IsNullOrWhiteSpace(guest.AllowedSubEventsIdsCommaList))
@@ -61,18 +91,21 @@ namespace EventQR.Areas.Scanner.Controllers
 
             await _context.CheckIns.AddAsync(_checkin);
             await _context.SaveChangesAsync();
-
+           // var check= await _context.CheckIns.FindAsync(GuestId, EventId);
             return View();
         }
 
-
-        public async Task<IActionResult> AllowGuest7(string id)
+        /*public async Task<IActionResult> AllowGuest(string id)
         {
+            var DecryptString = EventQR.Common.Static.Variables.Decrypt(id);
             Guid guestId = Guid.Empty;
             Guid eventId = Guid.Empty;
-
-            var loggedInUserId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var guest = await _context.Guests.Include(g => g.MyEvent).Where(g => g.UniqueId == guestId && g.EventId == eventId).FirstOrDefaultAsync();
+            if (!string.IsNullOrWhiteSpace(DecryptString))
+            {
+                var ids = DecryptString.Split(",");
+                Guid.TryParse(ids[0], out guestId);
+                Guid.TryParse(ids[1], out eventId);
+            }
             var allowedSubEvents = guest.AllowedSubEventsIdsCommaList.Split(',');
             GuestCheckIn _checkin = new GuestCheckIn()
             {
@@ -87,8 +120,8 @@ namespace EventQR.Areas.Scanner.Controllers
 
             await _context.CheckIns.AddAsync(_checkin);
             await _context.SaveChangesAsync();
-            //  var check= await _context.CheckIns.FindAsync(GuestId, EventId);
+            //var check= await _context.CheckIns.FindAsync(GuestId, EventId);
             return View();
-        }
+        }*/
     }
 }
