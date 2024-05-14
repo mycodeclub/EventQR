e181371f1bccd68d84c88bef8c29fcb165a94690
Muskan Srivastava <ankitsahay1990@gmail.com>
2024-05-14 13:35:13 +0530

    added controller TicketTemplateController and InviteTemplate Controller

---
+++ EventQR/Areas/EventOrganizer/Controllers/TicketTemplateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventQR.Areas.EventOrganizer.Controllers
+{
+    public class TicketTemplateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
